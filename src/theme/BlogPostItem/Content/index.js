6f19a05340cbef158e596b2ff94397575f777ed9
juniors017 <valentin.chevoleau@orange.fr>
2025-09-05 01:33:55 +0200

    <BlueSky metadata={metadata} />

--- src/theme/BlogPostItem/Content/index.js
+++ src/theme/BlogPostItem/Content/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BlogPostItemContent from '@theme-original/BlogPostItem/Content';
 import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
 import SerieBanner from '@site/src/components/SerieBanner';
-
+import BlueSky from "@site/src/components/BlueSky/index.js";  
 export default function BlogPostItemContentWrapper(props) {
   const { metadata, isBlogPostPage } = useBlogPost();
   const serieName = metadata?.frontMatter?.serie;
@@ -12,7 +12,13 @@ export default function BlogPostItemContentWrapper(props) {
       {serieName && isBlogPostPage && (
         <SerieBanner serieName={serieName} />
       )}
+           
+
       <BlogPostItemContent {...props} />
+<div className="margin-top--xl">
+  <hr></hr>
+      <BlueSky metadata={metadata} />
+      </div>
     </>
   );
 }
