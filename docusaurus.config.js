a3f6eadb62bf3eca884c3074f0840b37d0f084de
juniors017 <valentin.chevoleau@orange.fr>
2025-09-23 23:42:12 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,7 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
+// @ts-check `@type` JSDoc annotations allow editor autocompletion and type
+// checking (when paired with `@ts-check`). There are various equivalent ways to
+// declare your Docusaurus config. See:
+// https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
 import pluginTagRoute from "./plugins/docusaurus-plugin-tag-route/index.cjs"
@@ -13,250 +12,309 @@ import remarkReplaceWords from "./plugins/remark-replace-words/index.js"
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'DOCUX',
-  tagline: 'Content Management System for Docusaurus',
-  favicon: 'img/docux.png',
-  
-  organizationName: 'DocuxLab', // Usually your GitHub org/user name.
-  projectName: 'docux-blog', // Usually your repo name.
-  url: 'https://docuxlab.com',
-  baseUrl: '/',
-  plugins: [
-  [pluginTagRoute, {}],
-   [pluginSeriesRoute, {}],
-   [simpleAnalytics, {}],
-    [
-      '@docusaurus/plugin-client-redirects',
-      {
-        redirects: [
-          // Ajoutez vos redirections ici, par exemple:
-          // {
-          //   from: '/ancienne-page',
-          //   to: '/nouvelle-page',
-          // },
+    title: 'DOCUX',
+    tagline: 'Content Management System for Docusaurus',
+    favicon: 'img/docux.png',
+
+    organizationName: 'DocuxLab', // Usually your GitHub org/user name.
+    projectName: 'docux-blog', // Usually your repo name.
+    url: 'https://docuxlab.com',
+    baseUrl: '/',
+    plugins: [
+        [
+            pluginTagRoute, {}
         ],
-        // Redirections automatiques basées sur une taxonomie courante
-        createRedirects: (existingPath) => {
-          // Redirection des URLs anciennes vers nouvelles
-          if (existingPath.includes('/blog/')) {
-            return [
-              existingPath.replace('/blog/', '/articles/'),
-              existingPath.replace('/blog/', '/posts/')
-            ].filter(redirect => redirect !== existingPath);
-          }
-          return undefined;
-        }
-      },
-    ],
-  ],
-  trailingSlash: true, // Assure la cohérence des URLs avec des barres obliques finales
-  customFields: {
-    blueSky: {
-      // This is the BlueSky handle as displayed in your BlueSky profile page
-      handle: "docuxlab.com",
-    },
-  },
-  // Configuration des URLs canoniques
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  
-  // Future flags pour préparer Docusaurus v4
-  future: {
-    v4: {
-      removeLegacyPostBuildHeadAttribute: true,
-      useCssCascadeLayers: true,
-    },
-    experimental_faster: {
-      swcJsLoader: true,
-      swcJsMinimizer: true,
-      swcHtmlMinimizer: true,
-      lightningCssMinimizer: true,
-      mdxCrossCompilerCache: true,
-    },
-    experimental_storage: {
-      type: 'localStorage',
-      namespace: true,
-    },
-  },
-
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-  
-  headTags: [
-    // Données structurées du site web
-    {
-      tagName: 'script',
-      attributes: {
-        type: 'application/ld+json',
-      },
-      innerHTML: JSON.stringify({
-        '@context': 'https://schema.org',
-        '@type': 'WebSite',
-        'name': 'DOCUX',
-        'url': 'https://docuxlab.com/',
-        'description': 'Un système de gestion de contenu basé sur Docusaurus',
-        'potentialAction': {
-          '@type': 'SearchAction',
-          'target': 'https://docuxlab.com/search?q={search_term_string}',
-          'query-input': 'required name=search_term_string'
-        }
-      }),
-    },
-    // Données structurées pour l'organisation
-    {
-      tagName: 'script',
-      attributes: {
-        type: 'application/ld+json',
-      },
-      innerHTML: JSON.stringify({
-        '@context': 'https://schema.org',
-        '@type': 'Organization',
-        'name': 'DOCUX',
-        'url': 'https://docuxlab.com/',
-        'logo': 'https://docuxlab.com/img/docux.png',
-        'sameAs': [
-          'https://github.com/Juniors017/docux-blog'
+        [
+            pluginSeriesRoute, {}
+        ],
+        [
+            simpleAnalytics, {}
+        ],
+        [
+            '@docusaurus/plugin-client-redirects', {
+                redirects: [
+                    // Ajoutez vos redirections ici, par exemple: {   from: '/ancienne-page',   to:
+                    // '/nouvelle-page', },
+                ],
+                // Redirections automatiques basées sur une taxonomie courante
+                createRedirects: (existingPath) => {
+                    // Redirection des URLs anciennes vers nouvelles
+                    if (existingPath.includes('/blog/')) {
+                        return [
+                            existingPath.replace('/blog/', '/articles/'),
+                            existingPath.replace('/blog/', '/posts/')
+                        ].filter(redirect => redirect !== existingPath);
+                    }
+                    return undefined;
+                }
+            }
         ]
-      }),
+    ],
+    trailingSlash: true, // Assure la cohérence des URLs avec des barres obliques finales
+    customFields: {
+        blueSky: {
+            // This is the BlueSky handle as displayed in your BlueSky profile page
+            handle: "docuxlab.com"
+        }
     },
-  ],
+    // Configuration des URLs canoniques
+    onBrokenLinks: 'warn',
+    onBrokenMarkdownLinks: 'warn',
 
-  presets: [
-  [
-    'classic',
-    /** @type {import('@docusaurus/preset-classic').Options} */
-    ({
-      docs: false,
-      blog: {
-        showReadingTime: true,
-        feedOptions: {
-          type: ['rss', 'atom'],
-          xslt: true,
+    // Future flags pour préparer Docusaurus v4
+    future: {
+        v4: {
+            removeLegacyPostBuildHeadAttribute: true,
+            useCssCascadeLayers: true
         },
-       remarkPlugins: [[remarkReplaceWords, "blog"]] ,
+        experimental_faster: {
+            swcJsLoader: true,
+            swcJsMinimizer: true,
+            swcHtmlMinimizer: true,
+            lightningCssMinimizer: true,
+            mdxCrossCompilerCache: true
+        },
+        experimental_storage: {
+            type: 'localStorage',
+            namespace: true
+        }
+    },
 
-        editUrl: 'https://github.com/Juniors017/docux-blog/tree/main/',
-        onInlineTags: 'warn',
-        onInlineAuthors: 'warn',
-        onUntruncatedBlogPosts: 'ignore',
-      },
-      pages: {
-        remarkPlugins: [[remarkReplaceWords, "pages"]],
-      },
-      theme: {
-        customCss: './src/css/custom.css',
-      },
-      sitemap: {
-        changefreq: 'weekly',
-        priority: 0.5,
-        ignorePatterns: ['/tags/**', '/search/**', '/404', '/404.html'],
-        filename: 'sitemap.xml',
-      },
-    }),
-  ],
-],
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you may
+    // want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en']
+    },
 
-  
-  themeConfig:
+    headTags: [
+        // Données structurées du site web
+        {
+            tagName: 'script',
+            attributes: {
+                type: 'application/ld+json'
+            },
+            innerHTML: JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'WebSite',
+                'name': 'DOCUX',
+                'url': 'https://docuxlab.com/',
+                'description': 'Un système de gestion de contenu basé sur Docusaurus',
+                'potentialAction': {
+                    '@type': 'SearchAction',
+                    'target': 'https://docuxlab.com/search?q={search_term_string}',
+                    'query-input': 'required name=search_term_string'
+                }
+            })
+        },
+        // Données structurées pour l'organisation
+        {
+            tagName: 'script',
+            attributes: {
+                type: 'application/ld+json'
+            },
+            innerHTML: JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'Organization',
+                'name': 'DOCUX',
+                'url': 'https://docuxlab.com/',
+                'logo': 'https://docuxlab.com/img/docux.png',
+                'sameAs': ['https://github.com/Juniors017/docux-blog']
+            })
+        }
+    ],
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: false,
+                blog: {
+                    showReadingTime: true,
+                    blogSidebarCount: 'ALL',
+                    blogSidebarTitle: 'All our articles',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    // editUrl: '
+                    feedOptions: {
+                        type: [
+                            'rss', 'atom'
+                        ],
+                        xslt: true
+                    },
+                    remarkPlugins: [
+                        [remarkReplaceWords, "blog"]
+                    ],
+
+                    editUrl: 'https://github.com/Juniors017/docux-blog/tree/main/',
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'ignore'
+                },
+                pages: {
+                    remarkPlugins: [
+                        [remarkReplaceWords, "pages"]
+                    ]
+                },
+                theme: {
+                    customCss: './src/css/custom.css'
+                },
+                sitemap: {
+                    changefreq: 'weekly',
+                    priority: 0.5,
+                    ignorePatterns: [
+                        '/tags/**', '/search/**', '/404', '/404.html'
+                    ],
+                    filename: 'sitemap.xml'
+                }
+            })
+        ]
+    ],
+
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      metadata: [
-        // Métadonnées générales
-        {name: 'keywords', content: 'docusaurus, blog, documentation, cms, react, gestion de contenu, markdown'},
-        {name: 'description', content: 'Un système de gestion de contenu basé sur Docusaurus pour simplifier la création de blogs et de documentation technique'},
-        {name: 'author', content: 'Docux'},
-        {name: 'robots', content: 'index, follow'},
-        
-        // Google Search Console - Remplacez VOTRE_CODE_VERIFICATION_GOOGLE par votre code réel
-        {name: 'google-site-verification', content: 'VOTRE_CODE_VERIFICATION_GOOGLE'},
-        
-        // Twitter Card data
-        {name: 'twitter:card', content: 'summary_large_image'},
-        {name: 'twitter:site', content: '@votre_compte_twitter'},
-        {name: 'twitter:title', content: 'DOCUX - CMS pour Docusaurus'},
-        {name: 'twitter:description', content: 'Un système de gestion de contenu basé sur Docusaurus pour simplifier la création de blogs et de documentation technique'},
-        {name: 'twitter:image', content: 'https://docuxlab.com/img/docusaurus-social-card.jpg'},
-        
-        // Open Graph data
-        {property: 'og:title', content: 'DOCUX - CMS pour Docusaurus'},
-        {property: 'og:type', content: 'website'},
-        {property: 'og:url', content: 'https://docuxlab.com/'},
-        {property: 'og:image', content: 'https://docuxlab.com/img/docusaurus-social-card.jpg'},
-        {property: 'og:description', content: 'Un système de gestion de contenu basé sur Docusaurus pour simplifier la création de blogs et de documentation technique'},
-        {property: 'og:site_name', content: 'DOCUX'},
-        {property: 'og:locale', content: 'fr_FR'},
-      ],
-      navbar: {
-        title: 'Docux',
-        style: 'dark',
-        logo: {
-          alt: 'Docux Blog Logo',
-          src: 'img/docux.png',
-        },
-        items: [
-       
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        metadata: [
+            // Métadonnées générales
+            {
+                name: 'keywords',
+                content: 'docusaurus, blog, documentation, cms, react, gestion de contenu, markdown'
+            }, {
+                name: 'description',
+                content: 'Un système de gestion de contenu basé sur Docusaurus pour simplifier la créati' +
+                        'on de blogs et de documentation technique'
+            }, {
+                name: 'author',
+                content: 'Docux'
+            }, {
+                name: 'robots',
+                content: 'index, follow'
+            },
 
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {to: '/series', label: 'Series', position: 'left'},
-          {to: '/thanks', label: 'Thanks', position: 'right'},
-          {to:'/repository',label: 'Repository', position: 'left',},
-          {
-            to: 'https://github.com/Juniors017/docux-blog',
-            label: 'GitHub',
-            position: 'right',
-          },
+            // Google Search Console - Remplacez VOTRE_CODE_VERIFICATION_GOOGLE par votre
+            // code réel
+            {
+                name: 'google-site-verification',
+                content: 'VOTRE_CODE_VERIFICATION_GOOGLE'
+            },
+
+            // Twitter Card data
+            {
+                name: 'twitter:card',
+                content: 'summary_large_image'
+            }, {
+                name: 'twitter:site',
+                content: '@votre_compte_twitter'
+            }, {
+                name: 'twitter:title',
+                content: 'DOCUX - CMS pour Docusaurus'
+            }, {
+                name: 'twitter:description',
+                content: 'Un système de gestion de contenu basé sur Docusaurus pour simplifier la créati' +
+                        'on de blogs et de documentation technique'
+            }, {
+                name: 'twitter:image',
+                content: 'https://docuxlab.com/img/docusaurus-social-card.jpg'
+            },
+
+            // Open Graph data
+            {
+                property: 'og:title',
+                content: 'DOCUX - CMS pour Docusaurus'
+            }, {
+                property: 'og:type',
+                content: 'website'
+            }, {
+                property: 'og:url',
+                content: 'https://docuxlab.com/'
+            }, {
+                property: 'og:image',
+                content: 'https://docuxlab.com/img/docusaurus-social-card.jpg'
+            }, {
+                property: 'og:description',
+                content: 'Un système de gestion de contenu basé sur Docusaurus pour simplifier la créati' +
+                        'on de blogs et de documentation technique'
+            }, {
+                property: 'og:site_name',
+                content: 'DOCUX'
+            }, {
+                property: 'og:locale',
+                content: 'fr_FR'
+            }
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Community',
+        navbar: {
+            title: 'Docux',
+            style: 'dark',
+            logo: {
+                alt: 'Docux Blog Logo',
+                src: 'img/docux.png'
+            },
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-             
+
+                {
+                    to: '/blog',
+                    label: 'Blog',
+                    position: 'left'
+                }, {
+                    to: '/series',
+                    label: 'Series',
+                    position: 'left'
+                }, {
+                    to: '/thanks',
+                    label: 'Thanks',
+                    position: 'right'
+                }, {
+                    to: '/repository',
+                    label: 'Repository',
+                    position: 'left'
+                }, {
+                    to: 'https://github.com/Juniors017/docux-blog',
+                    label: 'GitHub',
+                    position: 'right'
+                }
+            ]
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus'
+                        }, {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus'
+                        }
+                    ]
+                }, {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog'
+                        }, {
+                            label: 'GitHub',
+                            href: 'https://github.com/Juniors017/docux-blog'
+                        }, {
+                            label: 'Thanks',
+                            to: '/thanks'
+                        }
+                    ]
+                }
             ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/Juniors017/docux-blog',
-              },
-              {
-                label: 'Thanks',
-                to: '/thanks',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} DocuxLab Blog, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+            copyright: `Copyright © ${new Date().getFullYear()} DocuxLab Blog, Inc. Built with Docusaurus.`
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula
+        }
+    })
 };
 
 export default config;
