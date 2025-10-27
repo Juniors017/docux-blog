96695c6aae68fedbbf3e141789064bd14147af4a
Docux <97809069+Juniors017@users.noreply.github.com>
2025-10-28 00:37:27 +0100

    404

--- src/theme/NotFound/Content/index.js
+++ src/theme/NotFound/Content/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Content from '@theme-original/NotFound/Content';
+import styles from './styles.module.css';
 
 export default function ContentWrapper(props) {
   return (
-    <>test
-      <Content {...props} />
-    </>
+    <div className={styles.notFoundBg}>
+      <h1>404 -Page Not Found</h1>
+      <p>We could not find what you were looking for.
+
+Please contact the owner of the site that linked <br />
+you to the original URL and let them know their link is broken.</p>
+    </div>
   );
 }
