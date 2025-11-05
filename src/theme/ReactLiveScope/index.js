e9615ea02f669147c3044e080751a28daca61369
Docux <97809069+Juniors017@users.noreply.github.com>
2025-11-05 22:58:01 +0100

    fix

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,12 +1,21 @@
+import * as React from "react";
 
-import * as React from 'react';
-
-import Tooltip from '@site/src/components/Tooltip';
-import LogoIcon from '@site/src/components/LogoIcon';
-import Skill from '@site/src/components/Skill';
-import Contributor from '@site/src/components/Contributor';
-import TimeTimer, { FallbackBefore, FallbackAfter } from '@site/src/components/TimeTimer';
-
+// @ts-ignore
+import Tooltip from "@site/src/components/Tooltip";
+import LogoIcon from "@site/src/components/LogoIcon";
+import Skill from "@site/src/components/Skill";
+import Contributor from "@site/src/components/Contributor";
+import Columns from "@site/src/components/Columns";
+import TimeTimer, {
+  FallbackBefore,
+  FallbackAfter,
+} from "@site/src/components/TimeTimer";
+import Column from "@site/src/components/Column";
+import Card from "@site/src/components/Card";
+import CardBody from "@site/src/components/Card/CardBody";
+import CardFooter from "@site/src/components/Card/CardFooter";
+import CardHeader from "@site/src/components/Card/CardHeader";
+import CardImage from "@site/src/components/Card/CardImage";
 // Export only the symbols the react-live runtime needs to evaluate snippets.
 const {
   useState,
@@ -41,4 +50,13 @@ export default {
   TimeTimer,
   FallbackBefore,
   FallbackAfter,
+  Columns,
+  Column,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardImage,
+  // HTML elements
+  center: "center",
 };
